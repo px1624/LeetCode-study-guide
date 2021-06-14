92a48f60b31c5c0b5fa7eb2e481786a09ee87107
Howard <46689708+hcao10@users.noreply.github.com>
2021-06-14 08:11:16 +0800

    Update solution_hcao10.cpp

--- Week001/98/solution_hcao10.cpp
+++ Week001/98/solution_hcao10.cpp
@@ -12,6 +12,16 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-
+        recursivelyTraverse(root, INT_MIN, INT_MAX);
+    }
+    bool recursivelyTraverse(TreeNode* root, int leftNodeValue, int rightNodeValue){
+        if(root == nullptr){
+            return ture;
+        }
+        if((root -> val <= leftNodeValue)||( root -> val >= rightNodeValue)){
+            return flase;
+        }
+        return  recursivelyTraverse( root -> left, leftNodeValue,  root ->val) && recursivelyTraverse( root -> right, root -> val,  rightNodeValue)
+        
     }
 };
