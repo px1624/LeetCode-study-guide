016468b3af9f4066907456c19680c7a280e66cf8
Howard <46689708+hcao10@users.noreply.github.com>
2021-06-15 22:37:32 +0800

    Update solution_hcao10.cpp

--- Week001/3/solution_hcao10.cpp
+++ Week001/3/solution_hcao10.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
+        int maxlen=0, k=-1;
+        vector<int> vec(128,-1);
+        for (int i=0;i<s.size();i++){
+            if(vec[int(s[i])]>k){
+                k=vec[int(s[i])];
+                vec[int(s[i])]=i;
+            }
+            else{
+                vec[int(s[i])]=i;
+                maxlen=max(maxlen,i-k);
+            }
+        }
+        return maxlen;
     }
 };
